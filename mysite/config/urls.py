87c38f5687cb4d65cbaadd4b40e23dab87bb4b5b
heetaeyang <htyang90@gmail.com>
2020-07-13 19:36:57 -0700

    Initial commit

---
+++ mysite/config/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import include, path
+from pybo import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('pybo/', include('pybo.urls')),
+]
