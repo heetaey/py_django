b4baa256e019acfad4e3d8287da1224689daff74
heetaeyang <htyang90@gmail.com>
2020-07-16 16:09:03 -0700

    Adds login and logout feature

--- mysite/config/settings.py
+++ mysite/config/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'pybo.apps.PyboConfig',
+    'common.apps.CommonConfig',
 
     'django.contrib.admin',
     'django.contrib.auth',
@@ -123,3 +124,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
