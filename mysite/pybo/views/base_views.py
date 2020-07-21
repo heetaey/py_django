2bc5bab015e83deafce0c8a813435d9474259a08
heetaeyang <htyang90@gmail.com>
2020-07-21 16:30:25 -0700

    Adds searching

--- mysite/pybo/views/base_views.py
+++ mysite/pybo/views/base_views.py
@@ -1,5 +1,6 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
+from django.db.models import Q
 
 from ..models import Question
 
@@ -8,11 +9,21 @@ def index(request):
     """
     Load pybo list
     """
+
     page = request.GET.get('page', '1')
+    kw = request.GET.get('kw', '') # Keyword for searching
     question_list = Question.objects.order_by('-create_date')
+    if kw:
+        question_list = question_list.filter(
+            Q(subject__icontains=kw) |  # Search subject
+            Q(content__icontains=kw) |  # Search content
+            Q(author__username__icontains=kw) |  # Search the Author
+            Q(answer__author__username__icontains=kw)  # Search the author's answer
+        ).distinct()
+
     paginator = Paginator(question_list, 10)
     page_obj = paginator.get_page(page)
-    context = {'question_list': page_obj}
+    context = {'question_list': page_obj, 'page': page, 'kw': kw}
     return render(request, 'pybo/question_list.html', context)
 
 
